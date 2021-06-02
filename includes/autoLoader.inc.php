46280e8c29f1e1636753d33bede41ca06f772ec3
Mr-Apollo <53540010+Mr-Apollo@users.noreply.github.com>
2021-06-02 16:19:37 +0200

    Add files via upload

--- includes/autoLoader.inc.php
+++ includes/autoLoader.inc.php
@@ -3,12 +3,15 @@
 spl_autoload_register('autoLoader');
     
     function autoLoader($className) {
-
+        //Folder Path
         $file_path = "class/";
+        //After the class name
         $file_extension = ".class.php";
+        //Before the className underline
         $file_underline = "_";
+        //Full Path To The File
         $fullpath = $file_path . $file_underline . $className . $file_extension;
-
+        //Error Handeling
         if (!file_exists($fullpath)) {
             return false;
         }
